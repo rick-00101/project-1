78e8bd539fce4bacd40dceccd33768da19aa3b75
rick-00101 <thakur34980@gmail.com>
2026-06-26 23:58:16 +0530

    daily

--- app/routes/topbar.tsx
+++ app/routes/topbar.tsx
@@ -23,14 +23,20 @@ export default function Topbar(
       <div className="flex w-full h-15 justify-end items-center p-2 ">
         <div className="mr-auto">
           <Link to="/" aria-label="Home">
-        <svg viewBox="0 0 100 100" className="h-8 w-8 fill-primary">
-          <circle cx="50" cy="50" r="40" />
-        </svg>
-      </Link>
+            <svg viewBox="0 0 100 100" className="h-8 w-8 fill-primary">
+              <circle cx="50" cy="50" r="40" />
+            </svg>
+          </Link>
         </div>
         <div className="mr-8">
           <InputGroup className="w-[300px]">
-            <InputGroupInput  value={prop.searchQuery} onChange={(e)=>{prop.setsearchQuery(e.target.value)}} placeholder="Search..." />
+            <InputGroupInput
+              value={prop.searchQuery}
+              onChange={(e) => {
+                prop.setsearchQuery(e.target.value);
+              }}
+              placeholder="Search..."
+            />
             <InputGroupAddon></InputGroupAddon>
           </InputGroup>
         </div>
